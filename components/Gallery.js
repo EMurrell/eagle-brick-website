48f03c817b6a760091e7391ce35c9282cc5bbb71
EMurrell <82235988+EMurrell@users.noreply.github.com>
2022-06-13 14:25:40 -0400

    ready for review

---
+++ components/Gallery.js
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+
+export default function Gallery() {
+  return (
+    <section id='Gallery' className='w-full'>
+      <div className='flex flex-col lg:flex-row'>
+        <div className='w-full p-2 lg:w-1/2'>
+          <Image
+            src='/after2.jpg'
+            alt='photo of chimney repair'
+            height={500}
+            width={500}
+            layout='responsive'
+          />
+        </div>
+        <div className='w-full p-2 pt-0 lg:pt-2 lg:w-1/2'>
+          <Image
+            src='/angle2.jpg'
+            alt='photo of chimney repair'
+            height={500}
+            width={500}
+            layout='responsive'
+          />
+        </div>
+      </div>
+      <div className='flex flex-col lg:flex-row'>
+        <div className='w-full p-2 pt-0 lg:w-1/2 lg:pt-2'>
+          <Image
+            src='/atint.jpg'
+            alt='photo of tinted bricks'
+            height={500}
+            width={500}
+            layout='responsive'
+          />
+        </div>
+        <div className='w-full p-2 pt-0 lg:pt-2 lg:w-1/2'>
+          <Image
+            src='/4bricks.jpg'
+            alt='photo of new bricks'
+            height={500}
+            width={500}
+            layout='responsive'
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     fontFamily: {
-      sans: ["Montserrat"],
-      serif: ["Playfair Display"],
+      sans: ['Montserrat'],
+      serif: ['Playfair Display'],
     },
     extend: {
       backgroundImage: {
         hero: "url('/black-bricks.jpg')",
-        quote: "url('/shovel.jpg')",
+        quote: "url('/bgbricks.jpg')",
       },
       colors: {
-        highlight1: "#138BC5",
+        highlight1: '#138BC5',
       },
     },
   },
